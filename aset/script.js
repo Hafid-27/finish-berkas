af968fefa3acca0a731d3bc398f77d16d79f82ae
Hafid Robbi Assifa <68275385+Hafid-27@users.noreply.github.com>
2020-09-18 19:01:17 +0700

    proyek
    finish

---
+++ aset/script.js
@@ -0,0 +1,7 @@
+let date= new Date();
+let name="Hafid Robbi Assifa";
+let village="Karawang, Jawa Barat";
+document.getElementById("year").innerHTML=date.getFullYear();
+document.getElementById("name").innerHTML=name;
+document.getElementById("village").innerHTML=village;
+alert("selamat datang!");
